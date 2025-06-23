2f73f2d52b98184cecf784c392e7717ee15edabd
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-06-24 07:43:18 +0900

    init: initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.solution(3);
+        String result2 = solution.solution(4);
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}
