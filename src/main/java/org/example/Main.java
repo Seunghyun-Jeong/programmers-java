8d97d3777ae4738ac3fb42bb8ed146d3361e1dee
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-06-25 10:56:55 +0900

    feat: 약수의 개수와 덧셈 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String result = solution.solution(3);
-        String result2 = solution.solution(4);
+        int result = solution.solution(13, 17);
+        int result2 = solution.solution(24, 27);
         System.out.println(result);
         System.out.println(result2);
     }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,17 +1,23 @@
 package org.example;
 
 public class Solution {
-    public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                sb.append("수");
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count % 2 == 0) {
+                answer += i;
             } else {
-                sb.append("박");
+                answer -= i;
             }
         }
-
-        return sb.toString();
+        return answer;
     }
 }
