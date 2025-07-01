3ef4e16d720d18a00f26e760c14016a8d9919d36
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-02 07:24:28 +0900

    feat: 같은 숫자는 싫어 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,18 +1,35 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public int[] solution(int []arr) {
+        ArrayList<Integer> answerList = new ArrayList<>();
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        if (arr.length > 0) {
+            answerList.add(arr[0]);
         }
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != answerList.get(answerList.size() - 1)) {
+                answerList.add(arr[i]);
+            }
+        }
+
+        return answerList.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
+        int[] result1 = sol.solution(arr1);
+        System.out.println(Arrays.toString(result1));
+
+        int[] arr2 = {4, 4, 4, 3, 3};
+        int[] result2 = sol.solution(arr2);
+        System.out.println(Arrays.toString(result2));
     }
 }
