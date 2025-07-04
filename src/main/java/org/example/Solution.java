997dd1dcfa5e9680e7b3cfba98635501047060c9
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-04 18:14:52 +0900

    feat: 크기가 작은 부분문자열 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,38 +1,39 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int pLength = p.length();
 
-    public int getGCD(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
+        long pValue = Long.parseLong(p);
+
+        for (int i = 0; i <= t.length() - pLength; i++) {
+            String subStr = t.substring(i, i + pLength);
+            long subStrValue = Long.parseLong(subStr);
+
+            if (subStrValue <= pValue) {
+                answer++;
+            }
         }
-        return a;
-    }
-
-    public int[] solution(int n, int m) {
-        int[] answer = new int[2];
-
-        answer[0] = getGCD(n, m);
-        answer[1] = (n / answer[0]) * m;
-
         return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int n1 = 3;
-        int m1 = 12;
-        int[] result1 = sol.solution(n1, m1);
-        System.out.println(Arrays.toString(result1));
+        String t1 = "3141592";
+        String p1 = "271";
+        int result1 = sol.solution(t1, p1);
+        System.out.println(result1);
 
-        int n2 = 2;
-        int m2 = 5;
-        int[] result2 = sol.solution(n2, m2);
-        System.out.println(Arrays.toString(result2));
+        String t2 = "500220839878";
+        String p2 = "7";
+        int result2 = sol.solution(t2, p2);
+        System.out.println(result2);
+
+        String t3 = "10203";
+        String p3 = "15";
+        int result3 = sol.solution(t3, p3);
+        System.out.println(result3);
     }
 }
