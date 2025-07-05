d7166a195eb488949348a7482eff555d1fc06c92
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-05 19:26:04 +0900

    feat: 예산 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,18 +1,19 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Solution {
-    public int solution(String t, String p) {
+    public int solution(int[] d, int budget) {
         int answer = 0;
-        int pLength = p.length();
 
-        long pValue = Long.parseLong(p);
+        Arrays.sort(d);
 
-        for (int i = 0; i <= t.length() - pLength; i++) {
-            String subStr = t.substring(i, i + pLength);
-            long subStrValue = Long.parseLong(subStr);
-
-            if (subStrValue <= pValue) {
+        for (int i = 0; i < d.length; i++) {
+            if (budget >= d[i]) {
+                budget -= d[i];
                 answer++;
+            } else {
+                break;
             }
         }
         return answer;
@@ -21,19 +22,14 @@ public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        String t1 = "3141592";
-        String p1 = "271";
-        int result1 = sol.solution(t1, p1);
+        int[] d1 = {1, 3, 2, 5, 4};
+        int budget1 = 9;
+        int result1 = sol.solution(d1, budget1);
         System.out.println(result1);
 
-        String t2 = "500220839878";
-        String p2 = "7";
-        int result2 = sol.solution(t2, p2);
+        int[] d2 = {2, 2, 3, 3};
+        int budget2 = 10;
+        int result2 = sol.solution(d2, budget2);
         System.out.println(result2);
-
-        String t3 = "10203";
-        String p3 = "15";
-        int result3 = sol.solution(t3, p3);
-        System.out.println(result3);
     }
 }
