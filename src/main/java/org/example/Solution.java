faf93ee3c1b27e6de74d0893aeee9483fa13dd96
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-06 12:51:09 +0900

    feat: 3진법 뒤집기 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,35 +1,26 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Solution {
-    public int solution(int[] d, int budget) {
-        int answer = 0;
+    public int solution(int n) {
+        String ternary = Integer.toString(n, 3);
 
-        Arrays.sort(d);
+        StringBuilder reversedTernary = new StringBuilder(ternary).reverse();
 
-        for (int i = 0; i < d.length; i++) {
-            if (budget >= d[i]) {
-                budget -= d[i];
-                answer++;
-            } else {
-                break;
-            }
-        }
+        int answer = Integer.parseInt(reversedTernary.toString(), 3);
+
         return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int[] d1 = {1, 3, 2, 5, 4};
-        int budget1 = 9;
-        int result1 = sol.solution(d1, budget1);
+        int n1 = 45;
+        int result1 = sol.solution(n1);
         System.out.println(result1);
 
-        int[] d2 = {2, 2, 3, 3};
-        int budget2 = 10;
-        int result2 = sol.solution(d2, budget2);
+        int n2 = 125;
+        int result2 = sol.solution(n2);
         System.out.println(result2);
+
     }
 }
