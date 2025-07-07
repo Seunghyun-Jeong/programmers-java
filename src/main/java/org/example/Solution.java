60de12857f835df64dc52586ea0a2ed92be55f2b
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-07 13:20:13 +0900

    feat: 삼총사 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,12 +1,19 @@
 package org.example;
 
 public class Solution {
-    public int solution(int n) {
-        String ternary = Integer.toString(n, 3);
+    public int solution(int[] number) {
+        int answer = 0;
+        int n = number.length;
 
-        StringBuilder reversedTernary = new StringBuilder(ternary).reverse();
-
-        int answer = Integer.parseInt(reversedTernary.toString(), 3);
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
 
         return answer;
     }
@@ -14,13 +21,16 @@ public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int n1 = 45;
-        int result1 = sol.solution(n1);
+        int[] number1 = {-2, 3, 0, 2, -5};
+        int result1 = sol.solution(number1);
         System.out.println(result1);
 
-        int n2 = 125;
-        int result2 = sol.solution(n2);
+        int[] number2 = {-3, -2, -1, 0, 1, 2, 3};
+        int result2 = sol.solution(number2);
         System.out.println(result2);
 
+        int[] number3 = {-1, 1, -1, 1};
+        int result3 = sol.solution(number3);
+        System.out.println(result3);
     }
 }
