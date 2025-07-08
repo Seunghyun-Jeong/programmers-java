c81b6425f57e8e25dd09aca13cf3deedcbe6ed25
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-08 13:25:08 +0900

    feat: 이상한 문자 만들기 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,36 +1,37 @@
 package org.example;
 
 public class Solution {
-    public int solution(int[] number) {
-        int answer = 0;
-        int n = number.length;
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] words = s.split(" ", -1);
 
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            StringBuilder processedWord = new StringBuilder();
+
+            for (int j = 0; j < word.length(); j++) {
+                char ch = word.charAt(j);
+                if (j % 2 == 0) {
+                    processedWord.append(Character.toUpperCase(ch));
+                } else {
+                    processedWord.append(Character.toLowerCase(ch));
                 }
+            }
+            answer.append(processedWord);
+
+            if (i < words.length - 1) {
+                answer.append(" ");
             }
         }
 
-        return answer;
+        return answer.toString();
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int[] number1 = {-2, 3, 0, 2, -5};
-        int result1 = sol.solution(number1);
+        String s1 = "try hello world";
+        String result1 = sol.solution(s1);
         System.out.println(result1);
-
-        int[] number2 = {-3, -2, -1, 0, 1, 2, 3};
-        int result2 = sol.solution(number2);
-        System.out.println(result2);
-
-        int[] number3 = {-1, 1, -1, 1};
-        int result3 = sol.solution(number3);
-        System.out.println(result3);
     }
 }
