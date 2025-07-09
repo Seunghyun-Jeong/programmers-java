6d26b9ed29239c7e8148ab1869a363d3912fb6a0
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-09 13:28:24 +0900

    feat: 최소직사각형 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,37 +1,39 @@
 package org.example;
 
 public class Solution {
-    public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        String[] words = s.split(" ", -1);
+    public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int maxHeight = 0;
 
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            StringBuilder processedWord = new StringBuilder();
+        for (int[] size : sizes) {
+            int w = size[0];
+            int h = size[1];
 
-            for (int j = 0; j < word.length(); j++) {
-                char ch = word.charAt(j);
-                if (j % 2 == 0) {
-                    processedWord.append(Character.toUpperCase(ch));
-                } else {
-                    processedWord.append(Character.toLowerCase(ch));
-                }
-            }
-            answer.append(processedWord);
+            int currentMax = Math.max(w, h);
+            int currentMin = Math.min(w, h);
 
-            if (i < words.length - 1) {
-                answer.append(" ");
-            }
+            maxWidth = Math.max(maxWidth, currentMax);
+            maxHeight = Math.max(maxHeight, currentMin);
         }
 
-        return answer.toString();
+        return maxWidth * maxHeight;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        String s1 = "try hello world";
-        String result1 = sol.solution(s1);
+        int[][] sizes1 = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        int result1 = sol.solution(sizes1);
         System.out.println(result1);
+
+        int[][] sizes2 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+        int result2 = sol.solution(sizes2);
+        System.out.println(result2);
+
+        // 테스트 케이스 3: 문제 예시
+        int[][] sizes3 = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
+        int result3 = sol.solution(sizes3);
+        System.out.println(result3);
+
     }
 }
