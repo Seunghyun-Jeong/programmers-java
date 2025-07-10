7aaa57886d3c4547065654bbee6a64cb5d37b934
Seunghyun-Jeong <dkskakdgka@gmail.com>
2025-07-10 19:18:52 +0900

    feat: 가장 가까운 같은 글자 구현

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,39 +1,38 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Solution {
-    public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
 
-        for (int[] size : sizes) {
-            int w = size[0];
-            int h = size[1];
+        HashMap<Character, Integer> lastSeen = new HashMap<>();
 
-            int currentMax = Math.max(w, h);
-            int currentMin = Math.min(w, h);
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
 
-            maxWidth = Math.max(maxWidth, currentMax);
-            maxHeight = Math.max(maxHeight, currentMin);
+            if (lastSeen.containsKey(currentChar)) {
+                answer[i] = i - lastSeen.get(currentChar);
+            } else {
+                answer[i] = -1;
+            }
+
+            lastSeen.put(currentChar, i);
         }
 
-        return maxWidth * maxHeight;
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int[][] sizes1 = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
-        int result1 = sol.solution(sizes1);
-        System.out.println(result1);
+        String s1 = "banana";
+        int[] result1 = sol.solution(s1);
+        System.out.println(Arrays.toString(result1));
 
-        int[][] sizes2 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
-        int result2 = sol.solution(sizes2);
-        System.out.println(result2);
-
-        // 테스트 케이스 3: 문제 예시
-        int[][] sizes3 = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
-        int result3 = sol.solution(sizes3);
-        System.out.println(result3);
-
+        String s2 = "foobar";
+        int[] result2 = sol.solution(s2);
+        System.out.println(Arrays.toString(result2));
     }
 }
